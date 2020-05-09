e4f773c4b93da5d9b3735bc96ed401f031d5b1b1
moonblade <moonblade168@gmail.com>
2020-05-09 22:59:28 +0530

    moved watermarker to docker

--- src/watermarker/db.js
+++ src/watermarker/db.js
@@ -19,11 +19,11 @@
 //     },
 //   }),
 // };
-const PROTO_PATH = `${__dirname}/../common/document.proto`;
+const PROTO_PATH = `${__dirname}/document.proto`;
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 const grpcPromise = require('grpc-promise');
-const config = require('../common/config');
+const docService = 'documentservice:3002';
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
@@ -34,7 +34,7 @@ const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
 
 const { DocumentService } = grpc.loadPackageDefinition(packageDefinition);
 const client = new DocumentService(
-  config.docService,
+  docService,
   grpc.credentials.createInsecure(),
 );
 grpcPromise.promisifyAll(client);
